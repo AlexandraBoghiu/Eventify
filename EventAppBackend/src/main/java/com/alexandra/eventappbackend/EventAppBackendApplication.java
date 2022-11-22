054eab6c0464076dfd48cecb769aa0fda6efc71e
AlexandraBoghiu <aboghiu2001@gmail.com>
2022-11-22 20:24:04 +0200

    Init commit

---
+++ EventAppBackend/src/main/java/com/alexandra/eventappbackend/EventAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alexandra.eventappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventAppBackendApplication.class, args);
+    }
+
+}
---
+++ EventAppBackend/src/test/java/com/alexandra/eventappbackend/EventAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandra.eventappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
